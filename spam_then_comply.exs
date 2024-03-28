157734c0de9f022a1b4422a8de140be40024570e
cognivore <66186054+cognivore@users.noreply.github.com>
2024-03-28 00:47:22 +0000

    Update spam_then_comply.exs

--- spam_then_comply.exs
+++ spam_then_comply.exs
@@ -9,7 +9,7 @@ alias CachexTransactionInvalidation, as: CTI
 
 Logger.info "We will spam twice and then comply with the 3 second rule."
 Logger.info "This trigger two spam detection events, as after the second `increment`, the duration of cache validity is 3_250 ms."
-Logger.info "Final result of the counter should be 1, not 2."
+Logger.info "Final result of the counter should be 2, not 3."
 
 Logger.debug "1. #{CTI.get()}"
 CTI.increment()
